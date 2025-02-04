fde895e2fbf0e00e32310095aca204b0107ac789
hody00 <hodaya060@gmail.com>
2025-02-04 21:31:20 +0200

    fix maven tests

--- myapp/src/test/java/com/myapp/AppTest.java
+++ myapp/src/test/java/com/myapp/AppTest.java
@@ -1,20 +1,93 @@
 package com.myapp;
 
-import static org.junit.Assert.assertTrue;
-
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class AppTest {
+
+    private static final String PROPERTIES_FILE = "app.properties";
+    private static boolean isLastTest = false;
+
+    @Before
+    public void setUp() {
+        if (!isLastTest) {
+            deletePropertiesFile();
+        }
+    }
+
+    @After
+    public void tearDown() {
+        if (!isLastTest) {
+            deletePropertiesFile();
+        }
+    }
+
+    private void deletePropertiesFile() {
+        File file = new File(PROPERTIES_FILE);
+        if (file.exists()) {
+            file.delete();
+        }
+    }
+
+    private void createPropertiesFile(String content) throws IOException {
+        FileWriter writer = new FileWriter(PROPERTIES_FILE);
+        writer.write(content);
+        writer.close();
+    }
+
+    private String runApp(String... args) throws IOException {
+        String jarFilePath = System.getProperty("jarFilePath", "target/myapp-1.1.1.jar");
+        ProcessBuilder builder = new ProcessBuilder("java", "-jar", jarFilePath);
+        if (args.length > 0) {
+            for (String arg : args) {
+                builder.command().add(arg);
+            }
+        }
+
+        Process process = builder.start();
+        Scanner scanner = new Scanner(process.getInputStream()).useDelimiter("\\A");
+        return scanner.hasNext() ? scanner.next() : "";
+    }
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testWithoutPropertiesFile() throws IOException {
+        String output = runApp();
+        assertTrue(output.contains("Hello World! from: Hodaya"));
+    }
+
+    @Test
+    public void testWithoutPropertiesFileWithCliArgs() throws IOException {
+        String output = runApp("test1", "test2");
+        assertTrue(output.contains("Hello World! from: Hodaya"));
+        assertTrue(output.contains("Hello World! from: test1 test2"));
+    }
+
+    @Test
+    public void testWithInvalidPropertiesFile() throws IOException {
+        createPropertiesFile("test.message=Test1 Test2");
+        String output = runApp();
+        assertTrue(output.contains("Hello World! from: Hodaya"));
+    }
+
+    @Test
+    public void testWithValidPropertiesFileAndCliArgs() throws IOException {
+        createPropertiesFile("app.message=Test1 Test2");
+        String output = runApp("cli1", "cli2");
+        assertTrue(output.contains("Hello World! from: Hodaya"));
+        assertTrue(output.contains("Hello World! from: Test1 Test2"));
+        assertTrue(output.contains("Hello World! from: cli1 cli2"));
+    }
+
+    @Test
+    public void restorePropertiesFile() throws IOException {
+        isLastTest = true;
+        createPropertiesFile("app.message=App Properties");
     }
 }
